bf1f495e95a9bc4af36aefec0ef47276028e12e3
codetestmg <13461280+codetestmg@users.noreply.github.com>
2020-01-15 01:05:07 +0530

    Updates

---
+++ precache-manifest.b039baaf966567be7fc518645df0dc8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fe40931d2c6aea5acac56d2e506c8fc",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "894ad0bd829a4d3259f4",
+    "url": "/reactapp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e11659dd89ce40dca5f0",
+    "url": "/reactapp/static/js/2.a5baf2ab.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/reactapp/static/js/2.a5baf2ab.chunk.js.LICENSE"
+  },
+  {
+    "revision": "894ad0bd829a4d3259f4",
+    "url": "/reactapp/static/js/main.f8ca2cb4.chunk.js"
+  },
+  {
+    "revision": "b4c0d33ca834fba2f582",
+    "url": "/reactapp/static/js/runtime-main.83117efd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
